e992bbc012a29ebb77dd253391e70c75f0a21ae2
subhradeep333 <codewithsubhradeep3@gmail.com>
2026-06-25 11:17:34 +0530

    Typecasting added in the repo

---
+++ Type casting/explicite_typecasting.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class explicite_typecasting{
+    public static void main(String[] args){
+        double d1 = 9.99;
+        int number = (int) d1;
+
+        System.out.println(d1);
+        System.out.println(number);
+    }
+}
---
+++ Type casting/implicite_typecasting.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class implicite_typecasting{
+    public static void main(String[] args){
+        int num = 6;
+        double d = num;
+        System.out.println(d);
+        System.out.println(num);
+    }
+}
